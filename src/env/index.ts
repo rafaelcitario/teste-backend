84f16e75950708e2bfb4d57d28c1ca287ec91862
Rafael Citario <contato.rafaelgomes@outlook.com>
2025-04-29 19:52:01 -0300

    install prisma

--- src/env/index.ts
+++ src/env/index.ts
@@ -3,6 +3,7 @@ import z from '../lib/zod';
 export const envSchema = z.object( {
   SERVER_HOST: z.string().default( "localhost" ),
   SERVER_PORT: z.string().transform( ( port ) => parseInt( port ) ).default( '4568' ),
+  DATABASE_URI: z.string()
 } );
 
 
