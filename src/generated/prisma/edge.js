87247e65e5f812861d59d73052925cebbb2bcf52
Rafael Citario <contato.rafaelgomes@outlook.com>
2025-04-30 22:57:06 -0300

    working

--- src/generated/prisma/edge.js
+++ src/generated/prisma/edge.js
@@ -233,11 +233,12 @@ const config = {
     "db"
   ],
   "activeProvider": "mysql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "mysql://root:senha_root_123@localhost:3306/wefit"
+        "value": null
       }
     }
   },
